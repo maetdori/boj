1f8dd26c5f8517e14e9a406d87c451293a41d5b2
LEE <odong2@ajou.ac.kr>
2021-08-04 11:39:05 +0900

    최대 힙

---
+++ src/자료구조/11279. 최대 힙/11279.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			if(num==0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+					continue;
+				}
+				System.out.println(pq.poll());
+				continue;
+			}
+
+			pq.offer(num);
+		}
+	}
+}
